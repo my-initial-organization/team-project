f90b17e87a08285e76241662e7ebdc826decebfa
janithamadubashana <madubashanajanitha@gmail.com>
2023-03-23 19:36:22 +0530

    load all subs method

--- src/main/java/lk/ijse/dep10/app/controller/ManageSubscribersViewController.java
+++ src/main/java/lk/ijse/dep10/app/controller/ManageSubscribersViewController.java
@@ -7,7 +7,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import lk.ijse.dep10.app.db.DBConnection;
 import lk.ijse.dep10.app.util.Subscriber;
+
+import java.sql.*;
 
 public class ManageSubscribersViewController {
 
@@ -36,11 +39,39 @@ public class ManageSubscribersViewController {
         Platform.runLater(() -> {
             btnNewSubscriber.fire();
         });
+        loadAllSubscribers();
 
+    }
+
+    private void loadAllSubscribers() {
+        try {
+            Connection connection= DBConnection.getInstance().getConnection();
+            Statement stm = connection.createStatement();
+            String sql="select * from Subscriber";
+            ResultSet rst = stm.executeQuery(sql);
+            while(rst.next()){
+                String id = rst.getString("id");
+                String name = rst.getString("name");
+                String address = rst.getString("address");
+                tblSubscribers.getItems().add(new Subscriber(id,name,address));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnDeleteOnAction(ActionEvent event) {
+        Subscriber selectedSubscriber = tblSubscribers.getSelectionModel().getSelectedItem();
+        Connection connection=DBConnection.getInstance().getConnection();
+        try {
+            PreparedStatement stm = connection.prepareStatement("delete from Subscriber where id=?");
+            stm.setString(1,selectedSubscriber.getId());
+            stm.executeUpdate();
+            tblSubscribers.getItems().remove(selectedSubscriber);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -49,6 +80,7 @@ public class ManageSubscribersViewController {
         generateNewId();
         txtName.clear();
         txtAddress.clear();
+        txtName.requestFocus();
 
 
     }
